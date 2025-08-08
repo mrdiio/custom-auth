38d5f799043f809df3e513820dac9d78d91bbe12
Dio <dionatsir@gmail.com>
2025-08-08 11:03:17 +0700

    feat: update environment variables and database connection settings
    - Modified .env.example to include DB_HOST and rearranged database connection variables.
    - Updated package.json to add new dependencies: @radix-ui/react-label, @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and tw-animate-css.
    - Updated pnpm-lock.yaml to reflect new dependencies and their versions.
    - Enhanced globals.css with new CSS variables and imported tw-animate-css.
    - Refactored Home component in page.tsx to conditionally render user information and buttons.
    - Updated drizzle.ts to use environment variables for database connection.
    - Added SignInForm and SignUpForm components for user authentication.
    - Created UI components for buttons, cards, inputs, and labels with Tailwind CSS styling.
    - Added utility functions for class name merging.

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,25 +1,25 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
 import { config } from 'dotenv'
+import { Pool } from 'pg'
 
 config({ path: '.env' })
 
-// export const client = new Pool({
-//   host: 'dev-nextjs.cxio4yoay8am.ap-southeast-3.rds.amazonaws.com',
-//   port: 5432,
-//   user: 'postgres',
-//   password: 'UntanMulia-2020',
-//   database: 'custom-auth-nextjs',
-// })
-
-// export const db = drizzle(client, { schema })
-
-export const db = drizzle({
-  schema,
-  connection: {
-    password: process.env.DB_PASSWORD,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-  },
+export const client = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
+
+export const db = drizzle(client, { schema })
+
+// export const db = drizzle({
+//   schema,
+//   connection: {
+//     password: process.env.DB_PASSWORD,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//   },
+// })
